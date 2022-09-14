43a014436a8bfdc60224656aeb2d1289c7879d4d
yeeunyuk <yy.eun6@gmail.com>
2022-09-15 00:38:32 +0900

    메인화면 배경 변경, 튜토리얼 추가

--- application/app/src/main/java/com/example/myapplication/ui/mainPage/HomeTutor.java
+++ application/app/src/main/java/com/example/myapplication/ui/mainPage/HomeTutor.java
@@ -10,12 +10,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.myapplication.R;
 
-public class MainFragment extends Fragment {
+public class HomeTutor extends Fragment {
 
     MainActivity mainActivity;
+    private ImageView Img_tutor;
+    private Button btn_tutor;
 
     @Override
     public void onAttach(Context context) {
@@ -33,14 +38,24 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        ViewGroup rootview = (ViewGroup)inflater.inflate(R.layout.f_diagnosis,container,false);
-        /*Button button3 = (Button)rootview.findViewById(R.id.button3);
-        button3.setOnClickListener(new View.OnClickListener() {
+        ViewGroup rootview = (ViewGroup)inflater.inflate(R.layout.home_main2,container,false);
+
+        Img_tutor = rootview.findViewById(R.id.Img_tutor);
+        btn_tutor = rootview.findViewById(R.id.btn_tutor);
+
+        btn_tutor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mainActivity.onChangeFragment(1);
+                Img_tutor.setImageResource(R.drawable.home_2);
             }
-        });*/
+        });
+        btn_tutor.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                Img_tutor.setImageResource(R.drawable.home_1);
+                return false;
+            }
+        });
 
         return rootview;
     }
