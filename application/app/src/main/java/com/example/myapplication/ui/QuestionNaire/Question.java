b8bd72341b488f9104bf74e835d936f069fad51a
yeeunyuk <yy.eun6@gmail.com>
2022-10-03 17:45:25 +0900

    drawer 수정, 문진표 등록 코드 작성

--- application/app/src/main/java/com/example/myapplication/ui/QuestionNaire/Question.java
+++ application/app/src/main/java/com/example/myapplication/ui/QuestionNaire/Question.java
@@ -3,39 +3,41 @@ package com.example.myapplication.ui.QuestionNaire;
 import com.google.gson.annotations.SerializedName;
 
 public class Question {
-    @SerializedName("QnName")
+    @SerializedName("petSerial")
+    public int petSerial;
+
+    @SerializedName("medicalFormName")
     private String QnName;
-    @SerializedName("QnDate")
+    @SerializedName("medicalFormDate")
     private String QnDate;
-    @SerializedName("QnTime")
+    @SerializedName("medicalFormTime")
     private String QnTime;
-    @SerializedName("QnReason")
+    @SerializedName("medicalFormQ1")
     private String QnReason;
-    @SerializedName("QnMedichine")
-    private String QnMedichine;
-
-    @SerializedName("dsEpidemiology")
-    private String dsEpidemiology;
-    @SerializedName("dsSymptom")
-    private String dsSymptom;
-    @SerializedName("dsDiagnosis")
-    private String dsDiagnosis;
-    @SerializedName("dsTreatment")
-    private String dsTreatment;
-    @SerializedName("dsPrevention")
-    private String dsPrevention;
-    @SerializedName("dsPrognosis")
-    private String dsPrognosis;
-
-    @SerializedName("QnEtc")
+    @SerializedName("medicalFormQ2")
+    private String QnDisease;
+    @SerializedName("medicalFormQ3")
+    private boolean QnMedichine;
+    @SerializedName("medicalFormQ4")
+    private String QnMedichine2;
+    @SerializedName("medicalFormQ5")
+    private boolean QnSurgery;
+    @SerializedName("medicalFormQ6")
+    private boolean QnExercise;
+    @SerializedName("medicalFormQ7")
     private String QnEtc;
 
-    public Question(String QnName, String QnDate, String QnTime, String QnReason, String QnMedichine, String QnEtc) {
+    public Question(int petSerial, String QnName, String QnDate, String QnTime, String QnReason, String QnDisease, boolean QnMedichine, String QnMedichine2, boolean QnSurgery, boolean QnExercise, String QnEtc) {
+        this.petSerial = petSerial;
         this.QnName = QnName;
         this.QnDate = QnDate;
         this.QnTime = QnTime;
         this.QnReason = QnReason;
+        this.QnDisease = QnDisease;
         this.QnMedichine = QnMedichine;
+        this.QnMedichine2 = QnMedichine2;
+        this.QnSurgery = QnSurgery;
+        this.QnExercise = QnExercise;
         this.QnEtc = QnEtc;
     }
 }
